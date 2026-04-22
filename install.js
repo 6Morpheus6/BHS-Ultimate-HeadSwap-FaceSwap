b35b158156f6492d08d9ef685cf9e1943bd85882
manat0912 <manaturipa@gmail.com>
2026-04-22 15:11:33 +1200

    Update install.js

--- install.js
+++ install.js
@@ -12,6 +12,55 @@ module.exports = {
     {
       method: "shell.run",
       params: {
+        message: [
+          "git lfs install",
+          "git clone https://huggingface.co/Alissonerdx/BFS-Best-Face-Swap"
+        ],
+        path: "app"
+      },
+      when: "{{!exists('app/BFS-Best-Face-Swap')}}"
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://huggingface.co/olesheva/head_swap_qwen_edit"
+        ],
+        path: "app"
+      },
+      when: "{{!exists('app/head_swap_qwen_edit')}}"
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/HumanAIGC/SwapAnyHead.git"
+        ],
+        path: "app"
+      },
+      when: "{{!exists('app/SwapAnyHead')}}"
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/visomaster/VisoMaster.git"
+        ],
+        path: "app"
+      },
+      when: "{{!exists('app/VisoMaster')}}"
+    },
+    {
+      method: "fs.download",
+      params: {
+        url: "https://github.com/iperov/DeepFaceLab/archive/refs/heads/master.zip",
+        dir: "app"
+      },
+      when: "{{!exists('app/DeepFaceLab-master.zip')}}"
+    },
+    {
+      method: "shell.run",
+      params: {
         venv: "env",
         path: "app",
         message: [
