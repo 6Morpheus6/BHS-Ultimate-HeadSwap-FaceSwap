e2e49ae348961744e48619286678fd03e07ff9e3
manat0912 <manaturipa@gmail.com>
2026-04-22 14:56:44 +1200

    Update 3 files

--- install.js
+++ install.js
@@ -1,40 +1,53 @@
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
         message: [
           "git clone https://github.com/Hanzyusuf/BHS-HeadSwap.git app",
         ]
-      }
+      },
+      when: "{{!exists('app')}}"
     },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
-          "uv pip install gradio devicetorch",
           "uv pip install -r requirements.txt"
         ]
       }
     },
-    // Delete this step if your project does not use torch
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // flashattention: true   // uncomment this line if your project requires flashattention
-          // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
-          // sageattention: true   // uncomment this line if your project requires sageattention
+          venv: "env",
+          path: "app"
         }
       }
     },
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: "app",
+        message: [
+          "python download_flux.py"
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "../../env",
+        path: "app/VisoMaster",
+        message: [
+          "python download_models.py"
+        ]
+      }
+    }
   ]
 }
