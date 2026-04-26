1eed7a11e184c3df854e4e4f286a63696528701e
6Morpheus6 <wanton-decay@gmx.net>
2026-04-26 06:00:10 +0200

    bundle ai

--- start.js
+++ start.js
@@ -1,14 +1,17 @@
 module.exports = {
+  requires: {
+    bundle: "ai",
+  },
   daemon: true,
   run: [
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        env: { },                   // Edit this to customize environment variables (see documentation)
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        env: { },
+        path: "app",
         message: [
-          "python main.py",    // Edit with your custom commands
+          "python main.py",
         ],
         on: [{
           "event": "/(http:\\/\\/[0-9.:]+)/",   
